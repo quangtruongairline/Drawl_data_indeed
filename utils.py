558e1a0d8d7252793aaf8e388e5f6f659195f548
Nguyen Quang Truong <139192880+QuangTruong-Nguyen@users.noreply.github.com>
2024-06-12 21:57:11 +0700

    u

--- utils.py
+++ utils.py
@@ -15,7 +15,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.core.os_manager import ChromeType
-
+import chromedriver_autoinstaller
 
 
 #______________________________
@@ -30,8 +30,36 @@ def init_driver():
     # chrome_options.add_argument("--headless")
     # driver = webdriver.Chrome(options=chrome_options)
     
-    driver_path = ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
-    driver = webdriver.Chrome(driver_path)
+    # driver_path = ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
+    # driver = webdriver.Chrome(driver_path)
+        
+    chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
+                                        # and if it doesn't exist, download it automatically,
+                                        # then add chromedriver to path
+
+    chrome_options = webdriver.ChromeOptions()    
+    # Add your options as needed    
+    options = [
+    # Define window size here
+    "--window-size=1200,1200",
+        "--ignore-certificate-errors"
+    
+        #"--headless",
+        #"--disable-gpu",
+        #"--window-size=1920,1200",
+        #"--ignore-certificate-errors",
+        #"--disable-extensions",
+        #"--no-sandbox",
+        #"--disable-dev-shm-usage",
+        #'--remote-debugging-port=9222'
+    ]
+
+    for option in options:
+        chrome_options.add_argument(option)
+
+        
+    driver = webdriver.Chrome(options = chrome_options)
+
     
     return driver
 
