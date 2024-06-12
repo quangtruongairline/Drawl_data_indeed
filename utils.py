4b0da8560d6d0af1b4d1c72181adc52f5ffad779
Nguyen Quang Truong <139192880+QuangTruong-Nguyen@users.noreply.github.com>
2024-06-13 00:00:51 +0700

    u

--- utils.py
+++ utils.py
@@ -42,12 +42,19 @@ def init_driver():
     # driver = webdriver.Edge(options=options)
 
 
+    # service = Service(ChromeDriverManager().install())
+    # options = webdriver.ChromeOptions()
+    # # options.add_argument('--headless')
+
+    # driver = webdriver.Chrome(service=service, options=options)
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--remote-debugging-port=9222')
     service = Service(ChromeDriverManager().install())
-    options = webdriver.ChromeOptions()
-    # options.add_argument('--headless')
-
-    driver = webdriver.Chrome(service=service, options=options)
-
+    driver = webdriver.Chrome(service=service, options=chrome_options)
     
     return driver
 
