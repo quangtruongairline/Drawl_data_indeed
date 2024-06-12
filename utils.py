e63136aface5a718deb85cd7ac7373c572ab11bb
Nguyen Quang Truong <139192880+QuangTruong-Nguyen@users.noreply.github.com>
2024-06-12 23:33:32 +0700

    u

--- utils.py
+++ utils.py
@@ -46,7 +46,6 @@ def init_driver():
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(service=service, options=options)
 
     
