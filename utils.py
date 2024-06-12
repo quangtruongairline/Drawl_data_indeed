3d4d6dca04f442cd09b3bf3834e387fd2a871ca0
Nguyen Quang Truong <139192880+QuangTruong-Nguyen@users.noreply.github.com>
2024-06-12 21:17:19 +0700

    -u

--- utils.py
+++ utils.py
@@ -11,7 +11,7 @@ from bs4 import BeautifulSoup
 import re
 import datetime
 import json
-
+from webdriver_manager.chrome import ChromeDriverManager
 
 #______________________________
 def init_driver():
@@ -19,8 +19,11 @@ def init_driver():
     # edge_options = webdriver.EdgeOptions()
     # options = Options()
     # driver = webdriver.Edge(options=edge_options)
-    cService = webdriver.ChromeService(executable_path='./chromedriver.exe')
-    driver = webdriver.Chrome(service = cService)
+    # cService = webdriver.ChromeService(executable_path='./chromedriver.exe')
+    # driver = webdriver.Chrome(service = cService)
+    
+    cService = webdriver.ChromeService(executable_path=ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=cService)
     
     return driver
 
